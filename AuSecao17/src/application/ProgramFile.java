06af208f08d3d2cd38d4b4da42903a71c77af919
MacedoMLucca <luccae.macedo@gmail.com>
2022-10-14 18:52:01 -0300

    Inicializando secao 17

---
+++ AuSecao17/src/application/ProgramFile.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ProgramFile {
+	public static void main(String[] args) {
+		
+		File file = new File("C:\\Users\\lucca.macedo\\Desktop\\Lucca\\apoioAulasUdemy\\Seçao 17 - Arquivos\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Error: "+e.getMessage());
+		}
+		finally {
+			if(sc != null){
+				sc.close();
+			}
+		}
+	}
+}
---
+++ AuSecao17/src/application/ProgramFileWriterBufferedWriter.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ProgramFileWriterBufferedWriter {
+
+	public static void main(String[] args) {
+		
+		String[] lines = new String[] {"Good morning", "Good aftrernoon", "Good night"};
+		
+		String path = "C:\\Users\\lucca.macedo\\Desktop\\Lucca\\apoioAulasUdemy\\Seçao 17 - Arquivos\\out.txt";
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+			
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+			catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+
